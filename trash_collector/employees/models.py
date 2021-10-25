75843273edce9a178a19ea062fb6acdd611299f5
JESSE ARROYO-ROMERO <jesseromero310@gmail.com>
2021-10-25 14:53:51 -0500

    Created employees create page along with the path and html file, created the edit function and html file.

--- trash_collector/employees/models.py
+++ trash_collector/employees/models.py
@@ -3,3 +3,15 @@ from django.db import models
 # Create your models here.
 
 # TODO: Create an Employee model with properties required by the user stories
+
+class Employee(models.Model):
+    name = models.CharField(max_length=50)
+    user = models.ForeignKey('accounts.User', blank=True, null=True, on_delete=models.CASCADE)
+    email = models.CharField(max_length=50)
+    address = models.CharField(max_length=50)
+    zip_code = models.CharField(max_length=5)
+    department = models.CharField(max_length=50)
+    role = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
--- trash_collector/employees/views.py
+++ trash_collector/employees/views.py
@@ -1,13 +1,59 @@
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
+from django.http.response import HttpResponseRedirect
 from django.shortcuts import render
 from django.apps import apps
+from django.urls.base import reverse
+
+from .models import Employee
 
 # Create your views here.
 
 # TODO: Create a function for each path created in employees/urls.py. Each will need a template as well.
 
-
+@login_required
 def index(request):
     # This line will get the Customer model from the other app, it can now be used to query the db for Customers
     Customer = apps.get_model('customers.Customer')
     return render(request, 'employees/index.html')
+
+@login_required
+def create(request):
+    logged_in_employee = request.user
+    if request.method == "POST":
+        name_from_form = request.POST.get('name')
+        address_from_form = request.POST.get('address')
+        zip_from_form = request.POST.get('zip_code')
+        department_from_form = request.POST.get('department')
+        role_from_form = request.POST.get('role')
+        email_from_form = request.POST.get('email')
+        new_employee = Employee(name=name_from_form, user=logged_in_employee, address=address_from_form, zip_code=zip_from_form, department=department_from_form, role=role_from_form, email=email_from_form)
+        new_employee.save()
+        return HttpResponseRedirect(reverse('employees:index'))
+    else:
+        return render(request, 'employees/create.html')
+
+@login_required
+def edit_profile(request):
+    logged_in_employee = request.user
+    logged_in_employee = Employee.objects.get(user=logged_in_employee)
+    if request.method == "POST":
+        name_from_form = request.POST.get('name')
+        address_from_form = request.POST.get('address')
+        zip_from_form = request.POST.get('zip_code')
+        department_from_form = request.POST.get('department')
+        role_from_form = request.POST.get('role')
+        email_from_form = request.POST.get('email')
+        logged_in_employee.name = name_from_form
+        logged_in_employee.address = address_from_form
+        logged_in_employee.zip_code = zip_from_form
+        logged_in_employee.department = department_from_form
+        logged_in_employee.role = role_from_form
+        logged_in_employee.email = email_from_form
+        logged_in_employee.save()
+        return HttpResponseRedirect(reverse('employees:index'))
+    else:
+        context = {
+            'logged_in_employee': logged_in_employee
+        }
+        return render(request, 'employees/edit_profile.html', context)
